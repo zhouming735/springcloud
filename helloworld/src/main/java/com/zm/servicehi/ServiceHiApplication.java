950896858af0ae0b53011ead2fd66304a2dce359
lomenzhou <zhouming735@163.com>
2018-01-28 23:50:59 +0800

    add helloworld project

---
+++ helloworld/src/main/java/com/zm/servicehi/ServiceHiApplication.java
@@ -0,0 +1,51 @@
+package com.zm.servicehi;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class ServiceHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHiApplication.class, args);
+	}
+	
+	private static  Logger logger = LoggerFactory.getLogger(ServiceHiApplication.class.getName());
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @RequestMapping("/hi")
+    public String callHome(){
+    	logger.info("calling trace service-hi  ");
+        return restTemplate.getForObject("http://localhost:8989/miya", String.class);
+    }
+    @RequestMapping("/info")
+    public String info(){
+    	logger.info("calling trace service-hi ");
+
+        return "i'm service-hi";
+
+    }
+
+    @Bean
+    public AlwaysSampler defaultSampler(){
+        return new AlwaysSampler();
+    }
+}
